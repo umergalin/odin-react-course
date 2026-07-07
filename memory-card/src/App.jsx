38b9be6c4a8c79d59b577bdd59ae694461e4b723
umergalin <91934841+umergalin@users.noreply.github.com>
2026-07-07 22:50:27 +0500

    wip: move person creation to Person.jsx

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,81 +1,97 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import { generateSeed } from './js/utils.js'
+import { generateSeed } from "./js/utils.js";
+import Person from "./Person.jsx";
 
-const CHAR_GEN_ARGS = {
-  skinColor: [
-    "#6f3e39",
-    "#7d4b40",
-    "#8c4a25",
-    "#975140",
-    "#995c30",
-    "#a46649",
-    "#b76e44",
-    "#c6775c",
-    "#e18d6f",
-    "#e59d65",
-    "#f39f7c",
-    "#ffb99e",
-    "#ffc5b7",
-    "#ffcba3",
-    "#ffe1d5",
-  ].map(hexCode => hexCode.replace("#", "")).join(","),
-};
-
-async function getImage(imgContainer) {
-  const baseUrl = "https://api.dicebear.com/10.x/dylan/svg";
-  const seed = generateSeed(8);
-
-  const query = [
-    `skinColor=${CHAR_GEN_ARGS.skinColor}`,
-    `seed=${seed}`,
-    `backgroundColor=`
-  ].join("&");
-
-  const url = `${baseUrl}?${query}`;
-
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`Can't get character image: ${response.status}`);
-    }
-
-    const blob = await response.blob();
-    const objUrl = URL.createObjectURL(blob); // CREATING MEMORY LEAK
-    imgContainer.src = objUrl;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
+const HALF_COLLS_COUNT = 2; // one side def prevents even grid
+const TOTAL_ROWS_COUNT = 4;
 
 function App() {
+  const getCellOwner = (row, col) => {
+    return occupiedCells.get(`${row},${col}`);
+  };
+
+  const addItemToCell = (item, row, col) => {
+    item.style.gridColumn = `${col} / ${col + 1}`;
+    item.style.gridRow = `${row} / ${row + 1}`;
+    setOccupiedCells((prev) => new Map(prev).set(`${row},${col}`, item));
+  };
+
+  const [gridItems, setGridItems] = useState(
+    Array.from(Array(TOTAL_ROWS_COUNT), () =>
+      Array(HALF_COLLS_COUNT * 2 + 1).fill(null), // adding extra coll for spacing
+    ),
+  );
+  const [occupiedCells, setOccupiedCells] = useState(new Set());
+
+  function addGridItem(row, col) {
+    const seed = generateSeed(8);
+
+    setGridItems((prev) => {
+      const next = [...prev];
+      next[row] = [...next[row]];
+      next[row][col] = seed; 
+      return next;
+    });
+
+    setOccupiedCells(prev => new Set(prev).add(`${row},${col}`));
+  }
+
+  function getFreeCells() {
+    const freeCells = [];
+
+    gridItems.forEach((row, rowIdx) => {
+      row.forEach((cell, colIdx) => {
+        if (cell === null) {
+          freeCells.push({ row: rowIdx, col: colIdx });
+        }
+      });
+    });
+
+    console.log(freeCells);
+  }
+
   return (
     <>
-    <div className="top">
-      <div>
-        <span>POINTS </span>
-        <span className="score">X</span>
+      <div className="top">
+        <div>
+          <span>POINTS </span>
+          <span className="score">X</span>
+        </div>
+        <div>
+          <span>RECORD </span>
+          <span className="record">Y</span>
+        </div>
       </div>
-      <div>
-        <span>RECORD </span>
-        <span className="record">Y</span>
+      <div className="center">
+        <div
+          className="arrangement"
+          style={{
+            "--half-colls-count": HALF_COLLS_COUNT,
+            "--total-rows-count": TOTAL_ROWS_COUNT,
+          }}
+        >
+          {gridItems.map((gridRow, rowIdx) =>
+            gridRow.map((seed, colIdx) => {
+              if (seed) return null;
+              return (
+                <Person key={seed} seed={seed} row={rowIdx} col={colIdx} />
+              );
+            }),
+          )}
+        </div>
+        <button onClick={() => addGridItem(1, 2)}>REDRUM</button>
+        <button onClick={() => getFreeCells()}>FREE CELLS</button>
       </div>
-    </div>
-    <div className="center">
-      <div className="arrangement">
-        
+      <div className="bottom">
+        <div>
+          <span>PAYMENT </span>
+          <span className="progress">X | X</span>
+        </div>
       </div>
-    </div>
-    <div className="bottom">
-      <div>
-        <span>PAYMENT </span>
-        <span className="progress">X | X</span>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ memory-card/src/Person.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+
+const PERSON_GEN_ARGS = {
+  skinColor: [
+    "#6f3e39",
+    "#7d4b40",
+    "#8c4a25",
+    "#975140",
+    "#995c30",
+    "#a46649",
+    "#b76e44",
+    "#c6775c",
+    "#e18d6f",
+    "#e59d65",
+    "#f39f7c",
+    "#ffb99e",
+    "#ffc5b7",
+    "#ffcba3",
+    "#ffe1d5",
+  ]
+    .map((hexCode) => hexCode.replace("#", ""))
+    .join(","),
+};
+
+async function fetchImageUrl(seed) {
+  const baseUrl = "https://api.dicebear.com/10.x/dylan/svg";
+
+  const query = [
+    `skinColor=${PERSON_GEN_ARGS.skinColor}`,
+    `seed=${seed}`,
+    `backgroundColor=`,
+  ].join("&");
+
+  const url = `${baseUrl}?${query}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Can't get character image: ${response.status}`);
+    }
+
+    const blob = await response.blob();
+    const imgUrl = URL.createObjectURL(blob); // CREATING MEMORY LEAK
+    return imgUrl;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function Person({ seed, row, col }) {
+    const [imgUrl, setimgUrl] = useState(null);
+
+    useEffect(() => {
+      let objectUrl = null;
+      let ignoreResult = false;
+
+      fetchImageUrl(seed).then((url) => {
+        objectUrl = url;
+        if (!ignoreResult) {
+          setimgUrl(url);
+        } else {
+          URL.revokeObjectURL(url);
+        }
+      });
+
+      return () => {
+        ignoreResult = true;
+        if (objectUrl) {
+          URL.revokeObjectURL(objectUrl);
+        }
+      };
+    }, [seed]);
+
+    return (
+      <div
+        className="person"
+        style={{
+          gridRow: `${row} / ${row + 1}`,
+          gridColumn: `${col} / ${col + 1}`,
+        }}
+      >
+        {imgUrl && <img src={imgUrl} alt="character" />}
+      </div>
+    );
+}
+
+export default Person;
