5e342bccdbaaa00c098f3b152d6fe1f4c91bff23
umergalin <91934841+umergalin@users.noreply.github.com>
2026-07-18 23:00:53 +0500

    refactor: store persons in persons array instead of grid

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -29,32 +29,36 @@ function App() {
   const [round, setRound] = useState(0);
   const [showOverlay, setShowOverlay] = useState(false);
 
+  const [persons, setPersons] = useState([]);
+
   const [gridItems, setGridItems] = useState(
     Array.from(Array(TOTAL_ROWS_COUNT), () =>
       Array(HALF_COLLS_COUNT * 2 + 1).fill(null), // adding extra coll for spacing
     ),
   );
 
-  function addGridItem(rowIdx, colIdx) {
-    const seed = generateSeed(8);
-
+  function addPersonToGrid(rowIdx, colIdx) {
     setGridItems((prev) => {
       const next = [...prev];
       next[rowIdx] = [...next[rowIdx]];
-      next[rowIdx][colIdx] = { seed }; 
+      next[rowIdx][colIdx] = true; 
       return next;
     });
   }
 
-  function addRandomPerson() {
+  function createRandomPerson() {
     const freeCells = getFreeCells();
     if (freeCells.length === 0) {
       console.log("can't add person: no more space in grid");
       return;
     }
-    console.log(freeCells);
-    const cell = freeCells[Math.floor(Math.random() * freeCells.length)];
-    addGridItem(cell.row, cell.col); 
+
+    const [row, col] = freeCells[Math.floor(Math.random() * freeCells.length)];
+    const newPerson = { seed: generateSeed(8), row: row, col: col };
+
+    setPersons((prev) => [...prev, newPerson]);
+
+    addPersonToGrid(newPerson.row, newPerson.col); 
   }
 
   function removePerson(rowIdx, colIdx) {
@@ -72,7 +76,7 @@ function App() {
     gridItems.forEach((row, rowIdx) => {
       row.forEach((cell, colIdx) => {
         if (cell === null && colIdx !== HALF_COLLS_COUNT) {
-          freeCells.push({ row: rowIdx, col: colIdx });
+          freeCells.push([rowIdx, colIdx]);
         }
       });
     });
@@ -115,25 +119,19 @@ function App() {
             "--total-rows-count": TOTAL_ROWS_COUNT,
           }}
         >
-          {gridItems.map((gridRow, rowIdx) =>
-            gridRow.map((item, colIdx) => {
-              return (
-                item && (
-                  <Person
-                    key={item.seed}
-                    seed={item.seed}
-                    row={rowIdx}
-                    col={colIdx}
-                    clickHandle={removePerson}
-                  />
-                )
-              );
-            }),
-          )}
+          {persons.map(({ seed, row, col }) => (
+            <Person
+              key={seed}
+              seed={seed}
+              row={row}
+              col={col}
+              clickHandle={removePerson}
+            />
+          ))}
         </div>
-        <div className={`overlay ${showOverlay ? "show" : ""}`}></div>
+
         <div></div>
-        <button onClick={() => addRandomPerson()}>REDRUM</button>
+        <button onClick={() => createRandomPerson()}>REDRUM</button>
       </div>
       <div className="bottom">
         {!gameState.isPlaying && (
@@ -145,6 +143,9 @@ function App() {
             <span className="progress">X | X</span>
           </div>
         )}
+      </div>
+      <div className={`overlay ${showOverlay ? "show" : ""}`}>
+        <div className="new-persons-list"></div>
       </div>
     </>
   );
